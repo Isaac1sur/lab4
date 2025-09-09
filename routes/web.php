f6a939d7d228fb5caeeb9857cbf61264f13a1991
Isaac1sur <isaacsurci@gmail.com>
2025-09-09 12:09:16 -0400

    commit de la rama-lab4

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+use App\Http\Controllers\SaludoController;
+
+Route::get('/saludo', [SaludoController::class, 'index']);
+
+use App\Http\Controllers\SaludoIsaacController;
+
+Route::get('/saludo', [SaludoIsaacController::class, 'index']);
